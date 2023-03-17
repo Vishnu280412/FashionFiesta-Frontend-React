c4413e632a9cd3c786dff2f3effa215196f5e10d
Vishnu280412 <vishnuudaiyar28@gmail.com>
2023-03-17 10:36:14 +0530

    Just added features of stripe in doc.. and have linked some buttons to link of stripe for reports.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,6 +24,14 @@ const Sidebar = ({ side, closeSidebar }) => {
                     <i className="bi bi-bar-chart mr-2 inline-block text-lg"></i>
                     <Link to="/dashboard/categories" className="text-base capitalize">categories</Link>
                 </li>
+                <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
+                    <i className="bi bi-arrow-left-right mr-2 inline-block text-lg"></i>
+                    <a href="https://dashboard.stripe.com/test/payments" className="text-base capitalize">transactions / payments</a>
+                </li>
+                <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
+                    <i className="bi bi-clipboard-data mr-2 inline-block text-lg"></i>
+                    <a href="https://dashboard.stripe.com/test/reports/hub" className="text-base capitalize">financial reports</a>
+                </li>
             </ul>
         </div>
     )
